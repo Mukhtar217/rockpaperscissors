cd0987ed3ad0f84f8ab25e0043f2e57bf1c54acd
Mukhtar Mahdi <mukhtar.mahdi217@gmail.com>
2018-11-20 22:43:16 +0000

    RandomNumberGen

--- js/script.js
+++ js/script.js
@@ -4,8 +4,27 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-$ ("#shoot").click(function() {
-    $("#userChoice").text($("#input").val());
+$("#shoot").click(function(){
+    console.log("Test");
+    $("#computerChoice").empty();
+    var randomNumber = Math.random();
+    console.log(randomNumber);
+    
+    //Computer's Random Pick
+    if(randomNumber > 0.33) {
+        $("#computerChoice").html("Paper!"); 
+    }
+    
+    else if(randomNumber > 0.66) {
+        $("#computerChoice").html("Rock!"); 
+    }
+    
+     else {
+        $("#computerChoice").html("Scissors!"); 
+     }
+     console.log("Hello");
+     console.log($("#shoot").val());
+     $("#userChoice").html($("#input").val());
 });
 
 
