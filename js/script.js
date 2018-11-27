c60384506b6814da96e4fabcc99029da2508ac15
Mukhtar Mahdi <mukhtar.mahdi217@gmail.com>
2018-11-27 22:49:59 +0000

    FINISHHHHHHHH

--- js/script.js
+++ js/script.js
@@ -5,28 +5,65 @@
 //GLOBAL VARIABLES
 /* global $ */
 $("#shoot").click(function(){
-    console.log("Test");
     $("#computerChoice").empty();
+    $("#userChoice").empty();
     var randomNumber = Math.random();
     console.log(randomNumber);
+    var uC= $("#input").val();
+    var cC;
     
     //Computer's Random Pick
-    if(randomNumber > 0.33) {
-        $("#computerChoice").html("Paper!"); 
+    if(randomNumber > 0.66) {
+        $("#computerChoice").html("Paper!");
+         cC= "PAPER";
     }
     
-    else if(randomNumber > 0.66) {
+    else if(randomNumber > 0.33) {
         $("#computerChoice").html("Rock!"); 
+         cC="ROCK";
     }
     
      else {
-        $("#computerChoice").html("Scissors!"); 
+        $("#computerChoice").html("Scissors!");
+         cC="SCISSORS";
      }
-     console.log("Hello");
-     console.log($("#shoot").val());
-     $("#userChoice").html($("#input").val());
+     
+     //Uppercase
+     uC = uC.toUpperCase();
+     console.log(uC);
+     if (uC === "ROCK") {
+         $("#userChoice").html("Rock!"); 
+     }
+     else if (uC === "PAPER") {
+         $("#userChoice").html("Paper!"); 
+     }
+     else if (uC === "SCISSORS") {
+         $("#userChoice").html("Scissors!"); 
+     }
+     
+     //Winner
+    if(cC === "PAPER" && uC === "SCISSORS") {
+        $("#result").html("You Won");
+        $("#end").attr("src","http://m.memegen.com/icj089.jpg");
+    }
+    else if(cC === "SCISSORS" && uC === "ROCK") {
+        $("#result").html("You Won");
+        $("#end").attr("src","http://m.memegen.com/icj089.jpg");
+    }
+    else if(cC === "ROCK" && uC === "PAPER") {
+        $("#result").html("You Won");
+        $("#end").attr("src","http://m.memegen.com/icj089.jpg");
+    }
+    else if(cC === uC) {
+        $("#result").html("It's a tie");
+        $("#end").attr("src","https://media.makeameme.org/created/its-a-tie-lu8m3b.jpg");
+    }
+    else {
+        $("#result").html("You Lost");
+        $("#end").attr("src","https://media.makeameme.org/created/you-didnt-win-9w58id.jpg");
+    }
+    
+
 });
 
-
-// DOCUMENT READY FUNCTION BELOW
 
